2d1886f1b32a93aebc159878914c880a5a9a40b4
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-30 10:08:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     Player.change(LedSpriteProperty.X, -1)
 })
-input.onGesture(Gesture.ScreenUp, function () {
-    Player.change(LedSpriteProperty.Y, -1)
+input.onGesture(Gesture.LogoUp, function () {
+    Player.change(LedSpriteProperty.Y, 1)
 })
 input.onButtonPressed(Button.B, function () {
     Player.change(LedSpriteProperty.X, 1)
 })
 input.onGesture(Gesture.LogoDown, function () {
-    Player.change(LedSpriteProperty.Y, 1)
+    Player.change(LedSpriteProperty.Y, -1)
 })
 let Player: game.LedSprite = null
 let Enemy = game.createSprite(randint(0, 5), randint(0, 5))
 Player = game.createSprite(2, 2)
 basic.forever(function () {
     if (Player.isTouching(Enemy)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
         Enemy.set(LedSpriteProperty.X, randint(0, 5))
         Enemy.set(LedSpriteProperty.Y, randint(0, 5))
     }
